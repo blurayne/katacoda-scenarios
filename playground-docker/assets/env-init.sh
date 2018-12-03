37063af59e71674fcf8b73f880be00696bdc4c68
Markus Geiger <markus@refinedlabs.com>
2018-12-03 18:55:41 +0100

    Updated

--- playground-docker/assets/env-init.sh
+++ playground-docker/assets/env-init.sh
@@ -2,7 +2,7 @@
 clear
 _PWD=$(pwd)
 echo -n "Preparing environment..."
-( apt -y install bash-completion nano ) 1>/dev/null 2>&1 &
+( apt -y install bash-completion nano iputils-ping wget ) 1>/dev/null 2>&1 &
 ( curl -q -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz  ) 1>/dev/null 2>&1 &
 ( cd $HOME/.bin && curl https://getmic.ro | bash  ) 1>/dev/null  2>&1  &
 echo "export EDITOR=$HOME/.bin/micro" >> ~/.bashrc
