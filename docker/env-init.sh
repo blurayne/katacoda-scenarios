8e6a5e34bd0943d89f04e3b67d1910b0e00e6303
Markus Geiger <markus@refinedlabs.com>
2018-12-03 15:23:04 +0100

    Updated

--- docker/env-init.sh
+++ docker/env-init.sh
@@ -1,2 +1,4 @@
 curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz
 cd $USER/.bin && curl https://getmic.ro | bash
+apt update && apt install zsh
+chsh -s /bin/zsh $USER
