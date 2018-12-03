963ec217977c0fb644f811671ab2d929792013ea
Markus Geiger <markus@refinedlabs.com>
2018-12-03 15:30:03 +0100

    Updated

--- docker/env-init.sh
+++ docker/env-init.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
-
-install_assets() {
-    curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz
-}
-
-install_micro() {
-    cd $USER/.bin && curl https://getmic.ro | bash 
-    echo 'alias edit=micro ' >> ~/.bashrc
-}
-
-install_bash_completion() {
-  apt install bash-completion
-}
-
-install_assets &
-install_micro &
-install_bash_completion &
-wait 
+curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz
+cd $USER/.bin && curl https://getmic.ro | bash 
+echo 'alias edit=micro ' >> ~/.bashrc
+apt install bash-completion
