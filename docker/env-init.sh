98672bec99a0797e1427e6a0c8acc807b5f5b868
Markus Geiger <markus@refinedlabs.com>
2018-12-03 13:59:19 +0100

    Updated

--- docker/env-init.sh
+++ docker/env-init.sh
@@ -1,2 +1,4 @@
 touch Makefile
 touch docker-compose.yml
+curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook xz
+cd /home/scrapbook
