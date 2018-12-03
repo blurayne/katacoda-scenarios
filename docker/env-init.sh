4a6337b58d816a8abdf1ae4e88ecddaa15eb77f2
Markus Geiger <markus@refinedlabs.com>
2018-12-03 15:53:06 +0100

    Updated

--- docker/env-init.sh
+++ docker/env-init.sh
@@ -2,6 +2,6 @@
 ( apt -y install bash-completion ) &
 ( curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz ) &
 ( cd $USER/.bin && curl https://getmic.ro | bash ) &
-echo 'alias edit=micro ' >> ~/.bashrc
+echo "export EDITOR=$HOME/.bin/=micro" >> ~/.bashrc
 wait
 clear
