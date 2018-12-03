47ff803e66f544822d87a82fd10d0a759261d84c
Markus Geiger <markus@refinedlabs.com>
2018-12-03 17:11:04 +0100

    Updated

--- docker/assets/env-init.sh
+++ docker/assets/env-init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+_PWD=$(pwd)
 apt -y install bash-completion 
 curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz 
 cd $HOME/.bin && curl https://getmic.ro | bash 
 echo "export EDITOR=$HOME/.bin/=micro" >> ~/.bashrc
+cd $_PWD
 clear
 bash -i
