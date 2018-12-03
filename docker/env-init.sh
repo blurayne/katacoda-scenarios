56217e8024f07a17facb22fc472f267e7ec080bb
Markus Geiger <markus@refinedlabs.com>
2018-12-03 14:37:17 +0100

    Updated

--- docker/env-init.sh
+++ docker/env-init.sh
@@ -1,4 +1,4 @@
 touch Makefile
 touch docker-compose.yml
-curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/tutorial -xz
+curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz
 curl https://getmic.ro | bash && mv micro ~/.bin
