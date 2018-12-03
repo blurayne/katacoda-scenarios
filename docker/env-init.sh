a45a22c81f3b686fdca92445b828952921092d3b
Markus Geiger <markus@refinedlabs.com>
2018-12-03 15:34:22 +0100

    Updated

--- docker/env-init.sh
+++ docker/env-init.sh
@@ -1,4 +1,5 @@
+apt -y install bash-completion
 curl -L -o- https://github.com/blurayne/katacoda-scenarios/raw/master/assets/examples.tar.gz | tar -C /home/scrapbook/ -xz
 cd $USER/.bin && curl https://getmic.ro | bash 
 echo 'alias edit=micro ' >> ~/.bashrc
-apt install bash-completion
+
